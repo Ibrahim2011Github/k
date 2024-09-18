6b93e04973d5ce01bc9b7cce80f8685c4dafd81a
Ibrahim2011Github <ibragim.ajdynly@gmail.com>
2024-09-19 02:27:05 +0400

    python

---
+++ news/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    menu = ['Home', 'About', 'Contact', 'Settings']
+    return render(request, 'news/home.html', {'title': 'Home', "menu":menu })
+
+def about(request):
+    menu = ['Home', 'About', 'Contact', 'Settings']
+    return render(request, 'news/about.html',{'title': 'About', "menu":menu})
+
+def contact(request):
+    menu = ['Home', 'About', 'Contact', 'Settings']
+    return render(request, 'news/contact.html',{'title': 'Contact', "menu":menu})
